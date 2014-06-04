2d26f18663806fdc21a9178de0a711483b1af1c4
chelm <christopher.helm@gmail.com>
2014-06-04 15:45:46 -0600

    better sql parsing logic and tests

--- test/models/postgis-test.js
+++ test/models/postgis-test.js
@@ -202,6 +202,60 @@ describe('PostGIS Model Tests', function(){
         });
       });
       
+      it('should query data with AND filter', function(done){
+        var gKey = 'test:german:data';
+        var data = require('../fixtures/germany.json');
+
+        PostGIS.insert( gKey, { name: 'german-data', geomType: 'Point', features: data.features }, 0, function( error, success ){
+
+          should.not.exist(error);
+          success.should.equal( true );
+
+          PostGIS.select( gKey, { layer: 0, where: 'ID >= 2894 AND ID <= \'2997\''}, function(err, res){
+
+            should.not.exist(error);
+            res[0].features.length.should.equal(7);
+
+            PostGIS.remove(gKey+':0', function(err, result){
+              should.not.exist( err );
+
+              PostGIS.getInfo( gKey + ':0', function( err, info ){
+                should.exist( err );
+                done();
+              });
+            });
+          });
+          
+        });
+      });
+
+      it('should query data with many AND filters', function(done){
+        var gKey = 'test:german:data2';
+        var data = require('../fixtures/germany.json');
+
+        PostGIS.insert( gKey, { name: 'german-data', geomType: 'Point', features: data.features }, 0, function( error, success ){
+
+          should.not.exist(error);
+          success.should.equal( true );
+
+          PostGIS.select( gKey, { layer: 0, where: 'ID >= 2894 AND ID <= 2997 AND Land like \'%Germany%\' AND Art like \'%BRL%\'' }, function(err, res){
+
+            should.not.exist(error);
+            res[0].features.length.should.equal(2);
+
+            PostGIS.remove(gKey+':0', function(err, result){
+              should.not.exist( err );
+
+              PostGIS.getInfo( gKey + ':0', function( err, info ){
+                should.exist( err );
+                done();
+              });
+            });
+          });
+
+        });
+      });
+      
 
 });
 
