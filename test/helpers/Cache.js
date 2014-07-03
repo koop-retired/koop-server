da7684271113f8e4fab1b42fdc4577db2d4e878d
Norman Barker <norman@cloudant.com>
2014-07-03 14:03:17 -0600

    spatialite integration with koop

--- test/helpers/Cache.js
+++ test/helpers/Cache.js
@@ -1,9 +1,13 @@
 var Cache = require('../../lib/Cache.js'),
   PostGIS = require('../../lib/PostGIS.js'),
+  SQLite = require('../../lib/SQLite.js'),
   config = require('config');
 
 global.config = config;
 
-Cache.db = PostGIS.connect(config.db.test.postgis.conn);
+if (config.db.test.postgis)
+  Cache.db = PostGIS.connect(config.db.test.postgis.conn);
+else if (config.db.test.sqlite)
+  Cache.db = SQLite.connect(config.db.test.sqlite);
 
 module.exports = Cache;
