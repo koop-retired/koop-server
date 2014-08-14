512464b5b99ad6e3f68f367e4f503c301099ea3a
chelm <christopher.helm@gmail.com>
2014-08-14 13:58:53 -0600

    working on non-global pattern

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require("express"),
   fs = require('fs'),
   bodyParser = require('body-parser'),
-  koop = require('./lib');
+  Koop = require('./lib');
 
-module.exports = function(config) {
+module.exports = function( config ) {
   var app = express();
 
   // handle POST requests 
@@ -19,23 +19,31 @@ module.exports = function(config) {
   // register providers into the app
   // sets up models, routes -> controllers/handlers 
   app.register = function(provider){
+    // only register if the provider has a name 
     if ( provider.name ) {
+      
+      app[ provider.name ] = new provider.controller( Koop.Cache );
+    
       for (var route in provider.routes){
         var path = route.split(' ');
-        app[path[0]]( path[1], provider.controller[ 
+        app[path[0]]( path[1], app[ provider.name ][ 
           provider.routes[ route ].action 
         ]);
       }
-      global[provider.name] = provider.model;
     }
   };
 
-  if ( !global.config ){
-    global.config = config;
+   // Start the Cache DB with the conn string from config
+  if ( config.db ) {
+    if ( config.db.postgis ){
+      Koop.Cache.db = Koop.PostGIS.connect( config.db.postgis.conn );
+    } else if ( config.db.sqlite ) {
+      Koop.Cache.db = Koop.SQLite.connect( config.db.sqlite );
+    }
+  } else {
+    console.log('Exiting since no DB configuration found in config');
+    process.exit();
   }
-
-  global.config.defaultStyle = fs.readFileSync( __dirname + '/templates/renderers/style.mss', 'utf8' );
- 
 
   return app;
   
